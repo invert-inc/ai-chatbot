ea6e9efe25a7c6ae1d0da25975081eabc1e25e11
Suhas <jargnar@gmail.com>
2025-01-17 13:20:55 +0530

    GPT-4o as default

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -12,8 +12,14 @@ export const models: Array<Model> = [
     id: 'o1-preview',
     label: 'o1 preview',
     apiIdentifier: 'o1-preview',
-    description: 'For complex, multi-step tasks',
+    description: 'For complex reasoning tasks',
+  },
+  {
+    id: 'gpt-4o',
+    label: 'gpt-4o',
+    apiIdentifier: 'gpt-4o',
+    description: 'For general multi-step tasks',
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'o1-preview';
+export const DEFAULT_MODEL_NAME: string = 'gpt-4o';
