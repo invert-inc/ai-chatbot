aac0c647c8aaef68d25984608c1ca0587b68d46e
Suhas <jargnar@gmail.com>
2025-01-17 16:38:54 +0530

    use model without middleware

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,13 +1,10 @@
 import { openai } from '@ai-sdk/openai';
 import { azure } from '@ai-sdk/azure';
-import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
-import { customMiddleware } from './custom-middleware';
+// import { experimental_wrapLanguageModel as wrapLanguageModel } from 'ai';
+// import { customMiddleware } from './custom-middleware';
 
 export const customModel = (apiIdentifier: string) => {
-  return wrapLanguageModel({
-    model: azure(apiIdentifier, { simulateStreaming: true }),
-    middleware: customMiddleware,
-  });
+  return azure(apiIdentifier, { simulateStreaming: true });
 };
 
 export const imageGenerationModel = openai.image('dall-e-3');
